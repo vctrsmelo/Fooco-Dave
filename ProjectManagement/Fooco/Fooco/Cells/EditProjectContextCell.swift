616c46b4e6d438c658e31202eed949e8a1f80d91
Victor S Melo <victor.contas@outlook.com>
2017-11-14 16:45:23 -0200

    context collection view animated

--- ProjectManagement/Fooco/Fooco/Cells/EditProjectContextCell.swift
+++ ProjectManagement/Fooco/Fooco/Cells/EditProjectContextCell.swift
@@ -14,6 +14,10 @@ class EditProjectContextCell: UICollectionViewCell {
     @IBOutlet weak var mainBackground: UIView!
     @IBOutlet weak var shadowLayer: UIView!
     
+    private let heightIncreaseFactor : CGFloat = 40.0
+    static let originalSize = CGSize(width: 150, height: 100)
+    private var featuredPercentage: CGFloat?
+    
     private var _context: Context!
     var context: Context! {
         set {
@@ -29,8 +33,63 @@ class EditProjectContextCell: UICollectionViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    public func updateSize(cellFrame: CGRect, container: CGRect){
+        
+        let originalSize = EditProjectContextCell.originalSize
+        
+        let containerXCenter : CGFloat = container.origin.x + container.width/2
+        
+        let cellXCenter : CGFloat = cellFrame.origin.x + cellFrame.width/2
+        
+        let distance : CGFloat = abs(containerXCenter - cellXCenter)
+        
+        
+        //entre 0 e 100, sendo 100 = maior tamanho e 0 = menor tamanho
+        featuredPercentage = (1 - (distance/container.width))*100
+        
+        let newHeight = originalSize.height+(featuredPercentage!*heightIncreaseFactor)/100
+        let newWidth = (originalSize.width/originalSize.height) * newHeight
+        
+        
+        //size changes relating to current size
+        let increasedHeight = abs(self.frame.height - newHeight)
+        let increasedWidth = abs(self.frame.width - newWidth)
+        
+        
+        //position changes relating to current position
+        let newX = (self.frame.width < newWidth) ? self.frame.origin.x - increasedWidth/2 : self.frame.origin.x + increasedWidth/2
+        let newY = (self.frame.height < newHeight) ? self.frame.origin.y - increasedHeight : self.frame.origin.y + increasedHeight
+        
+        self.frame.size.width = CGFloat(newWidth)
+        self.frame.size.height = CGFloat(newHeight)
+        
+        self.frame.origin.x = newX
+        self.frame.origin.y = newY
+        
+        adjustShadow()
+    
+    }
+    
+    public func adjustShadow(){
+        
+        let ft = (featuredPercentage != nil) ? featuredPercentage! : 0.0
+        let defaultShadowRadius: CGFloat = 2
+        var finalShadowRadius: CGFloat = 4
+        
+        if(ft != 0.0){
+            
+            finalShadowRadius = defaultShadowRadius * ft / (defaultShadowRadius*10)
+            
+        }
+        
+        self.clipsToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.18
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowRadius = finalShadowRadius
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: 4).cgPath
+        
+        
     }
     
     override func awakeFromNib() {
--- ProjectManagement/Fooco/Fooco/Controllers/EditProjectTableViewController.swift
+++ ProjectManagement/Fooco/Fooco/Controllers/EditProjectTableViewController.swift
@@ -12,13 +12,24 @@ class EditProjectTableViewController: UITableViewController {
     
     @IBOutlet weak var contextsCollectionView: UICollectionView!
     
-    var selectedContextIndexPath: IndexPath? = nil
+    var selectedContext: Context? = nil
     
     override func viewDidLoad() {
         
         //set delegate and data source
         contextsCollectionView.delegate = self
         contextsCollectionView.dataSource = self
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 10, right: 20)
+        layout.itemSize = contextsCollectionView.frame.size
+        layout.minimumInteritemSpacing = 80
+        layout.minimumLineSpacing = 80
+        layout.scrollDirection = UICollectionViewScrollDirection.horizontal
+        contextsCollectionView.collectionViewLayout = layout
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         let nib = UINib(nibName: "EditProjectContextCell", bundle: nil)
         contextsCollectionView.register(nib, forCellWithReuseIdentifier: "contextCell")
@@ -36,9 +47,12 @@ extension EditProjectTableViewController: UICollectionViewDelegate, UICollection
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "contextCell", for: indexPath) as! EditProjectContextCell
         
-        if selectedContextIndexPath == nil {
-            updateSelectedContext(with: indexPath)
+        if selectedContext == nil {
+            updateSelectedContext(with: cell.context)
         }
+        
+        let cellFrame = contextsCollectionView.convert(cell.frame, to: contextsCollectionView.superview)
+        cell.updateSize(cellFrame: cellFrame, container: collectionView.frame)
         
         if indexPath.row >= User.sharedInstance.contexts.count {
             
@@ -51,29 +65,83 @@ extension EditProjectTableViewController: UICollectionViewDelegate, UICollection
         }
         
         return cell
+        
     }
     
-    private func updateSelectedContext(with indexPath: IndexPath) {
+    private func updateSelectedContext(with context: Context?) {
         
-      selectedContextIndexPath = indexPath
-        
+      selectedContext = context
         
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: EditProjectContextCell.originalSize.width, height: EditProjectContextCell.originalSize.height)
+    }
+
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        return CGSize(width: 174, height: 121)
+        if scrollView == contextsCollectionView {
+            for cell in contextsCollectionView.visibleCells {
+                
+                guard let contextCell = cell as? EditProjectContextCell else { return }
+                let cellFrame = contextsCollectionView.convert(contextCell.frame, to: contextsCollectionView.superview)
+                contextCell.updateSize(cellFrame: cellFrame, container: contextsCollectionView.frame)
+                
+            }
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
-        let totalCellWidth = 174 * collectionView.visibleCells.count
+        let totalCellWidth = EditProjectContextCell.originalSize.width * CGFloat(collectionView.visibleCells.count)
         let totalSpacingWidth = 10 * (collectionView.visibleCells.count - 1)
-        
-        let leftInset = (collectionView.frame.size.width - CGFloat(174 + 10)) / 2
+
+        let leftInset = (collectionView.frame.size.width - CGFloat(EditProjectContextCell.originalSize.width + 10)) / 2
         let rightInset = leftInset
-        
+
         return UIEdgeInsetsMake(0, leftInset, 0, rightInset)
+    
     }
+    
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        if scrollView == contextsCollectionView {
+            focusContextCell()
+        }
+        
+    }
+    
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        focusContextCell()
+    }
+    
+    private func focusContextCell() {
+        
+        let screenCenterX: CGFloat = contextsCollectionView.frame.origin.x + contextsCollectionView.frame.size.width/2.0
+        
+        guard var focusedCell: EditProjectContextCell = contextsCollectionView.visibleCells.first as? EditProjectContextCell else { return }
+        
+        let focusedCellAttributes = contextsCollectionView.layoutAttributesForItem(at: contextsCollectionView.indexPath(for: focusedCell)!)
+        let focusedCellFrame = contextsCollectionView.convert(focusedCellAttributes!.frame, to: contextsCollectionView.superview!)
+        var focusedCellCenterX = focusedCellFrame.origin.x + focusedCellFrame.size.width/2.0
+        
+        for cell in contextsCollectionView.visibleCells {
+            
+            let cellAttributes = contextsCollectionView.layoutAttributesForItem(at: contextsCollectionView.indexPath(for: cell)!)
+            let cellFrame = contextsCollectionView.convert(cellAttributes!.frame, to: contextsCollectionView.superview!)
+            let cellCenterX = cellFrame.origin.x + cellFrame.size.width/2.0
+            
+            if (abs(screenCenterX-focusedCellCenterX) > abs(screenCenterX-cellCenterX)) {
+                focusedCell = cell as! EditProjectContextCell
+                focusedCellCenterX = cellCenterX
+            }
+            
+        }
+        updateSelectedContext(with: focusedCell.context)
+        contextsCollectionView.scrollToItem(at: contextsCollectionView.indexPath(for: focusedCell)!, at: UICollectionViewScrollPosition.centeredHorizontally, animated: true)
+        
+    }
+    
 }
 
