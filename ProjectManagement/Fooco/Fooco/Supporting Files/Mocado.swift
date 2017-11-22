4afa3a8712bcc58d9023e5ec493ce667ab64bd08
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-11-22 18:50:19 -0200

    Made some Mocado parts private to be easier to use

--- ProjectManagement/Fooco/Fooco/Supporting Files/Mocado.swift
+++ ProjectManagement/Fooco/Fooco/Supporting Files/Mocado.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class Mocado {
     
+	static let context1: Context = Context(named: "context1", color: UIColor.contextColors().first!, projects: nil, minProjectWorkingTime: nil, maximumWorkingHoursPerProject: 3200)
     static var projects: [Project] = Mocado.populateProjects()
-    static let today: Date! = Date()
-    static let tomorrow: Date! = Date().addingTimeInterval(86_400)
+	
+    private static let today: Date! = Date()
+    private static let tomorrow: Date! = Date().addingTimeInterval(86_400)
     
-    static let context1: Context = Context(named: "context1", color: UIColor.contextColors().first!, projects: nil, minProjectWorkingTime: nil, maximumWorkingHoursPerProject: 3_200)
-    
-    static let defaultWeekday = Weekday(contextBlocks: [ContextBlock(timeBlock: TimeBlock(startsAt: Date(), endsAt: Date().addingTimeInterval(10_800)), context: context1)])
+    private static let defaultWeekday = Weekday(contextBlocks: [ContextBlock(timeBlock: TimeBlock(startsAt: Date(), endsAt: Date().addingTimeInterval(10_800)), context: context1)])
     
     static let defaultWeek = Week(sunday: defaultWeekday, monday: defaultWeekday, tuesday: defaultWeekday, wednesday: defaultWeekday, thursday: defaultWeekday, friday: defaultWeekday, saturday: defaultWeekday)
     
-    static private func populateProjects() -> [Project] {
+    private static func populateProjects() -> [Project] {
         
-        let proj1 = Project(named: "proj1", startsAt: today, endsAt: tomorrow, withContext: context1, importance: 1, totalTimeEstimated: 7_200)
+        let proj1 = Project(named: "proj1", startsAt: today, endsAt: tomorrow, withContext: context1, importance: 1, totalTimeEstimated: 7200)
         
         let proj2 = Project(named: "proj2", startsAt: today, endsAt: tomorrow, withContext: context1, importance: 1, totalTimeEstimated: 10_800)
         
-        return [proj1,proj2]
+        return [proj1, proj2]
         
     }
     
