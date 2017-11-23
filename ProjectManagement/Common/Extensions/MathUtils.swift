a7a4feffe105d1d3aefe363014e50028aa6f4d38
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-11-23 00:45:36 -0200

    Refactored some extensions and created view file for ActivityCard

--- ProjectManagement/Common/Extensions/MathUtils.swift
+++ ProjectManagement/Common/Extensions/MathUtils.swift
@@ -6,9 +6,21 @@
 //
 
 import CoreGraphics
+import Foundation
 
 extension CGPoint {
 	static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
 		return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+	}
+}
+
+extension TimeInterval {
+	func toString() -> String {
+		let formatter = DateComponentsFormatter()
+		formatter.allowedUnits = [.day, .hour, .minute]
+		formatter.unitsStyle = .abbreviated
+		formatter.maximumUnitCount = 2
+		
+		return formatter.string(from: self)!
 	}
 }
