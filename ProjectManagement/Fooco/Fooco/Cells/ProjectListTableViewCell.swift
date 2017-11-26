482accb46453d031470bc869b4b03e21ecc140d4
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-11-26 00:31:22 -0200

    Created files for the Project List

---
+++ ProjectManagement/Fooco/Fooco/Cells/ProjectListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProjectListTableViewCell.swift
+//  Fooco
+//
+//  Created by Rodrigo Cardoso Buske on 26/11/17.
+//
+
+import UIKit
+
+class ProjectListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
